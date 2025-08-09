cd0fe39c620a2a5f29058dce88e3da9fd2d1082b
Mustafa Tutkun <mustafatutkun30@gmail.com>
2025-08-09 19:42:14 +0300

    render settings

--- mul_blog/settings.py
+++ mul_blog/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
