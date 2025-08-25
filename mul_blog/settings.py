67c8a63e2eff7ca5a387519aa1da5debae27bc2c
Mustafa Tutkun <mustafatutkun30@gmail.com>
2025-08-25 18:32:11 +0300

    settings.py da ayarlamalar yapıldı.

--- mul_blog/settings.py
+++ mul_blog/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
-# os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
 
 ALLOWED_HOSTS = ['*']
 
