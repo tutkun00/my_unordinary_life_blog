317992d97537a1f0b2faf4a8d0f126a6d7e561c1
Mustafa Tutkun <mustafatutkun30@gmail.com>
2025-08-25 18:33:26 +0300

    settings.py ayarlamaları

--- mul_blog/settings.py
+++ mul_blog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['myunordinarylifeblog.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
